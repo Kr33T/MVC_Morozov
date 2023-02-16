23c32ac1954db76352521d8ab56c522a1933f772
am321 <am321@DESKTOP-5NCTJOV>
2023-02-16 19:13:15 +0300

    Добавьте файлы проекта.

---
+++ MVC_Morozov/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVC_Morozov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.resultTB = resultTB;
+            Model.operSignTB = operSignTB;
+
+            operCB.ItemsSource = Model.operCB_Text;
+        }
+
+        private void operCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.operSignTB_set = operCB.SelectedIndex;
+        }
+
+        private void solveBTN_Click(object sender, RoutedEventArgs e)
+        {
+            Model.firstTB = firstTB.Text;
+            Model.secondTB = secondTB.Text;
+            Model.operResult_set = operCB.SelectedIndex;
+        }
+    }
+}
---
+++ MVC_Morozov/Model.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MVC_Morozov
+{
+    internal class Model
+    {
+        public static string firstTB;
+        public static string secondTB;
+
+        public static List<string> operCB_Text = new List<string>() { "Сложение", "Вычитание", "Умножение", "Деление" };
+        public static List<string> operSignTB_Text = new List<string>() { "+", "-", "*", "/" };
+
+        public static TextBlock operSignTB = new TextBlock();
+        public static TextBox resultTB = new TextBox();
+
+        public static int operSignTB_set
+        {
+            set
+            {
+                operSignTB.Text = operSignTB_Text[value];
+            }
+        }
+
+        public static int operResult_set
+        {
+            set
+            {
+                try
+                {
+                    double first = Convert.ToDouble(firstTB);
+                    double second = Convert.ToDouble(secondTB);
+
+                    switch (value)
+                    {
+                        case 0:
+                            resultTB.Text = (first + second).ToString();
+                            break;
+                        case 1:
+                            resultTB.Text = (first - second).ToString();
+                            break;
+                        case 2:
+                            resultTB.Text = (first * second).ToString();
+                            break;
+                        case 3:
+                            if (second == 0)
+                            {
+                                resultTB.Text = "Деление на ноль невозможно";
+                            }
+                            else
+                            {
+                                resultTB.Text = (first / second).ToString();
+                            }
+                            break;
+                    }
+                }
+                catch
+                {
+                    resultTB.Text = "Проверьте корректность введенных данных";
+                }
+            }
+        }
+    }
+}
